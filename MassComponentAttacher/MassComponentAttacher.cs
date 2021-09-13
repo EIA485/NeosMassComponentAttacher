30cf619b5d64ac99c0da07fae5fb02116184764b
eia485 <eia485git@gmail.com>
2021-09-13 16:05:52 -0700

    Add project files.

---
+++ MassComponentAttacher/MassComponentAttacher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FrooxEngine;
+using FrooxEngine.UIX;
+namespace MassComponentAttacher
+{
+	[Category("Add-Ons")]
+	class MassComponentAttacher : Component, ICustomInspector
+	{
+		public readonly SyncRef<Slot> TargetSlot;
+		public readonly Sync<bool> Recursive;
+		public readonly SyncRef<Component> TemplateComponent;
+		public readonly Sync<bool> AttachAsNew;
+		[NameOverride("breakExternalReferences/dontRunOnAttachBehavior")]
+		public readonly Sync<bool> BreakStuff;
+		public void BuildInspectorUI(UIBuilder ui)
+		{
+			WorkerInspector.BuildInspectorUI(this, ui);
+			Button button = ui.Button("Attach Components", click);
+		}
+		protected override void OnAttach()
+        {
+			base.OnAttach();
+			TargetSlot.Target = this.Slot;
+        }
+		public void click(IButton button, ButtonEventData eventData)
+		{
+			if(TemplateComponent.Target==null)
+            {
+				button.LabelText = "<color=red>component can not be null";
+				return;
+            }
+			Type type = TemplateComponent.Target.GetType();
+			if (Recursive)
+			{
+				int i =0;
+				(TargetSlot.Target??this.Slot).ForeachChild((slot) =>
+				{
+					Attach(type, slot);
+					i++;
+				});
+				button.LabelText = $"attached {i} Components";
+			}
+			else
+			{
+				for (int i = 0; i < TargetSlot.Target.ChildrenCount; ++i)
+				{
+					Attach(type, TargetSlot.Target[i]);
+				}
+				button.LabelText = $"attatched {TargetSlot.Target.ChildrenCount} components";
+			}
+
+		}
+		private void Attach(Type type, Slot slot)
+		{
+			if (AttachAsNew.Value)
+				slot.AttachComponent(type, !BreakStuff.Value, null); 
+            else
+            {
+				typeof(Slot).GetMethod("DuplicateComponent").MakeGenericMethod(new Type[] { type }).Invoke(slot, new object[] { TemplateComponent.Target, BreakStuff.Value });
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# MassComponentAttacher
+A simple plugin to attach large numbers of components.
+
+## Installation
+1. download [MassComponentAttacher.dll](https://github.com/eia485/NeosMassComponentAttacher/releases/latest/download/MassComponentAttacher.dll)
+2. place MassComponentAttacher.dll into C:\Program Files (x86)\Steam\steamapps\common\NeosVR\Libraries
+3. add `-LoadAssembly "Libraries\MassComponentAttacher.dll"` to your neos launch options
